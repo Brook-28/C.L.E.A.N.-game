f8580ccbce6689d74154b4d2d2b165fcd3b2caca
Brook-28 <rajanas28n@gmail.com>
2026-08-05 23:22:04 +0300

    added basic weapon switching system. error in the arms scene couldnt locate weapons parse. error if i make the player appear with arms already equiped and then switch to the revolver error appears because of queue free

---
+++ c.l.e.a.n/weapons/arms/arms.gd
@@ -0,0 +1,28 @@
+extends Weapon
+
+@onready var sprite = $AnimatedSprite2D
+var mouse_pos: Vector2
+var angle
+
+func _process(_delta: float) -> void:
+	mouse_pos = get_global_mouse_position()
+	angle = rad_to_deg(global_position.direction_to(mouse_pos).angle())
+	
+	#weapon_direction()
+	
+
+func weapon_direction():
+	
+	#flip logic
+	if mouse_pos.x < global_position.x:
+		sprite.flip_h = true
+		sprite.position.x = -6
+		angle = 180.0 - angle
+		if angle > 180.0:
+			angle -= 360.0
+		
+	else:
+		sprite.flip_h = false
+		sprite.position.x = 6
+			
+	
